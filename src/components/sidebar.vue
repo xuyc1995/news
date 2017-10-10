ff0998c7d7800f8a1a43492325792adbc6eb6974
佳涛 应 <yjt15851481743@outlook.com>
2017-10-10 13:28:26 +0800

    ‘sliderbar完成

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,5 +1,33 @@
 <template>
-    <h1>sidebar</h1>
+    <div class="slidebar">
+      <img src="../assets/logo.png" />
+      <ul>
+        <li><a class="active" href=''>推荐</a></li>
+        <li><a href=''>热点</a></li>
+        <li><a href=''>图片</a></li>
+        <li><a href=''>科技</a></li>
+        <li><a href=''>社会</a></li>
+        <li><a href=''>娱乐</a></li>
+        <li><a href=''>游戏</a></li>
+        <li><a href=''>体育</a></li>
+        <li><a href=''>汽车</a></li>
+        <li><a href=''>财经</a></li>
+        <li>
+          <a href='' id="showmore">
+            更多
+            <ul>
+              <li><a href=''>更多1</a></li>
+              <li><a href=''>更多2</a></li>
+              <li><a href=''>更多3</a></li>
+              <li><a href=''>更多4</a></li>
+              <li><a href=''>更多5</a></li>
+              <li><a href=''>更多6</a></li>
+              <li><a href=''>更多7</a></li>
+            </ul>
+          </a>
+        </li>        
+      </ul>
+    </div>
 </template>
 
 <script>
@@ -7,7 +35,7 @@ export default {
   name: 'sidebar',
   data () {
     return {
-     
+      
     }
   }
 }
