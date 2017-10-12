2466599235fa42efe143d06dfa396650e9c14497
xuyc <xuyc@think-land.com>
2017-10-12 16:44:27 +0800

    完善登陆界面

--- src/components/loginmodal.vue
+++ src/components/loginmodal.vue
@@ -1,5 +1,29 @@
 <template>
-    <div class="login-container">
-        <div class="login-mask"></div>
+    <div class="login-container" >
+        <div class="login-mask" @click="quiteLogin"></div>
+        <div class="login-box">
+            <div class="title">用户登录</div>
+            <input type="email" placeholder="请输入邮箱地址" v-model="loginData.email">
+            <input type="password" placeholder="请输入密码" v-model="loginData.password">
+            <button>登录</button>
+        </div> 
     </div>
 </template>
+<script>
+    export default{
+        data(){
+            return{
+                loginData:{
+                    email:"",
+                    password:""
+                }
+            }
+        },
+        methods:{
+            quiteLogin:function(){
+                this.$emit('quiteLoginmodal')
+                // console.log("quitelogin"+'======'+this.loginData.password+'========'+this.loginData.email)
+            }
+        }
+    }
+</script>
--- src/page/homepage.vue
+++ src/page/homepage.vue
@@ -3,26 +3,46 @@
         <div class='main-header'>
             <router-link to='/regeister'>注册</router-link>
             <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
-            <a href="javascript:;">登录</a>
+            <a href="javascript:;" @click="showLogin">登录</a>
         </div>
         <div class='main-container'>
             <div class='sidebar-container'>
                 <sidebar></sidebar>
             </div>
-            <div class='homepage-right-container'>
+            <div class='homepage-right-container' ref="right">
                 <newslist></newslist>
             </div>
         </div>
+        <loginmodal v-if="showLoginmodal"  v-on:quiteLoginmodal='quite'></loginmodal>
     </div>
 </template>
 <script>
+    import loginmodal from '../components/loginmodal'
     import sidebar from '../components/sidebar'
     import newslist from '../components/newslist'
     export default{
-        components:{sidebar,newslist},
+        components:{sidebar,newslist,loginmodal},
         data(){
             return {
-
+                showLoginmodal:false,
+                scroll:""
+            }
+        },
+        mounted(){
+            window.addEventListener('scroll',this.handleScroll)
+        },
+        methods:{
+            quite:function(){
+                this.showLoginmodal=false
+            },
+            //显示登录窗口
+            showLogin:function(){
+                this.showLoginmodal=true
+            },
+            handleScroll:function(){
+                // this.scroll=document.body.scrollTop
+                // console.log(this.scroll)
+                console.log(this.$refs.right.scrollTop)
             }
         }
     }
